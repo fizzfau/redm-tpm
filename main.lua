fa8082ed444d980e87ee24fba8bd76bdc5c09b33
fizzfau <59974801+fizzfau@users.noreply.github.com>
2022-10-17 12:12:02 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,17 +1,9 @@
-Citizen.CreateThread(function()
-    while FH == nil do
-        FH = exports['fh-core']:getObject()
-        Citizen.Wait(3)
-    end
-end)
-
 RegisterCommand("tpm", function(source)
     local waypointCoords = GetWaypointCoords()
     for height = 1, 999 do
       Citizen.Wait(5)
       local retval, groundZ = GetGroundZAndNormalFor_3dCoord(waypointCoords.x, waypointCoords.y, height + 0.0)
       SetEntityCoords(PlayerPedId(), waypointCoords["x"], waypointCoords["y"], height + 0.0)
-      print(retval)
       if retval then
         SetEntityCoords(PlayerPedId(), waypointCoords["x"], waypointCoords["y"], height + 0.0)
         break
